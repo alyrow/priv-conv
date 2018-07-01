a66a280995386d2fe20270597110c87ea5988847
alyrow <36855289+alyrow@users.noreply.github.com>
2018-07-01 18:38:37 +0200

    Droidscript update

--- server.js
+++ server.js
@@ -1,10 +1,58 @@
-var mosca = require('mosca');
-var settings = {
-		port:1883
-		}
+var mosca = require('mosca')
+ 
+ 
+//here we start mosca
+var http     = require('http')
+  , httpServ = http.createServer()
+  , mosca    = require('mosca')
+  , server = new mosca.Server({});
 
-var server = new mosca.Server(settings);
+server.attachHttpServer(httpServ);
 
-server.on('ready', function(){
-console.log("ready");
-});
+httpServ.listen(3000);
+
+server.on('ready', setup);
+
+var authenticate = function(client, username, password, callback) {
+	var authorized = (username === 'user' && password.toString() === 'password');
+	if (authorized) client.user = username;
+	callback(null, authorized);
+} 
+
+// fired when the mqtt server is ready
+function setup() {
+	// Accepts the connection if the username and password are valid
+	server.authenticate = authenticate;
+	console.log('Mosca server is up and running')
+}
+ 
+// fired whena  client is connected
+server.on('clientConnected', function(client) {
+	console.log('client connected', client.id);
+	});
+ 
+// fired when a message is received
+server.on('published', function(packet, client) {
+	console.log('Published : ', packet.payload.toString());
+	});
+ 
+// fired when a client subscribes to a topic
+server.on('subscribed', function(topic, client) {
+	console.log('subscribed : ', topic);
+	});
+ 
+// fired when a client subscribes to a topic
+server.on('unsubscribed', function(topic, client) {
+	console.log('unsubscribed : ', topic);
+	});
+ 
+// fired when a client is disconnecting
+server.on('clientDisconnecting', function(client) {
+	console.log('clientDisconnecting : ', client.id);
+	});
+ 
+// fired when a client is disconnected
+server.on('clientDisconnected', function(client) {
+	console.log('clientDisconnected : ', client.id);
+	});
+
