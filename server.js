42e581fd38d2951264382473e18770f044e5bf37
alyrow <36855289+alyrow@users.noreply.github.com>
2018-07-01 19:09:48 +0200

    Up

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
-var http = require('http');
-var url = require('url');
+var http = require('http'); 
 
-var server = http.createServer(function(req, res) {
-    var page = url.parse(req.url).pathname;
-    console.log(page);
-    res.writeHead(200, {"Content-Type": "text/plain"});
-    res.write('Bien le bonjour');
-    res.end();
+var url = require('url'); 
+
+var querystring = require('querystring'); 
+
+var server = http.createServer(function(req, res) { 
+
+var params = querystring.parse(url.parse(req.url).query); 
+
+res.writeHead(200, {"Content-Type": "text/plain"}); 
+
+if ('prenom' in params && 'nom' in params) { 
+
+res.write('Vous vous appelez ' + params['prenom'] + ' ' + params['nom']); 
+
+} 
+
+else { 
+
+res.write('Vous devez bien avoir un prénom et un nom, non ?'); 
+
+} 
+
+res.end(); 
+
 });
+
+
 server.listen(process.env.PORT);
